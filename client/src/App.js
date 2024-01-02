c97f4e00cdef7e27402ae66dce165b397deb42bb
Moises <julianbrb23@gmail.com>
2024-01-02 17:07:08 +0100

    Added UI

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,44 @@
 import React from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
+/**Import all components */
+
+import Username from './components/Username.js'
+import Password from './components/Password.js'
+import Register from './components/Register.js'
+import Profile from './components/Profile.js'
+import Recovery from './components/Recovery.js'
+import Reset from './components/Reset.js'
+import Pagenotfound from './components/Username.js'
+
+
+
 /**root routes */
 
 const router = createBrowserRouter([
   {
     path : "/",
-    element : <div>Route Route</div>
+    element : <Username></Username>
   },
   {
     path : "/register",
-    element : <div>Register</div>
+    element : <Register></Register>
+  },
+  {
+    path : "/password",
+    element : <Password></Password>
+  },  {
+    path : "/profile",
+    element : <Profile></Profile>
+  },  {
+    path : "/recovery",
+    element : <Recovery></Recovery>
+  },  {
+    path : "/reset",
+    element : <Reset></Reset>
+  },  {
+    path : "*",
+    element : <Pagenotfound></Pagenotfound>
   },
 ]);
 
